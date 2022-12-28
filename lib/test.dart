acf89e97d7de11d60fe1226fe30c90d77ee5cb49
drplxrd <kamangasteven00@gmail.com>
2022-12-28 17:08:30 +0200

    first commit

---
+++ lib/test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_mvvc_course/app/app.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  void updateAppState() {
+    MyApp.instance.appState = 12;
+  }
+
+  void getAppState() {
+    print(MyApp.instance.appState); //Will show 10
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
