51b229dbfb522e21278d7bea8cc38022bba0735e
drplxrd <kamangasteven00@gmail.com>
2023-01-02 14:12:37 +0200

    pi

--- lib/presentation/onbording/onboarding.dart
+++ lib/presentation/onbording/onboarding.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:udemy_mvvc_course/presentation/resources/assets_manager.dart';
 import 'package:udemy_mvvc_course/presentation/resources/color_manager.dart';
 import 'package:udemy_mvvc_course/presentation/resources/strings_manager.dart';
+import 'package:udemy_mvvc_course/presentation/resources/values_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -10,15 +15,106 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          AppStrings.onBoardingTitle1,
+          AppStrings.onBoardingSubTitle1,
+          ImageAssets.onboardingLogo1,
+        ),
+        SliderObject(
+          AppStrings.onBoardingTitle2,
+          AppStrings.onBoardingSubTitle2,
+          ImageAssets.onboardingLogo2,
+        ),
+        SliderObject(
+          AppStrings.onBoardingTitle3,
+          AppStrings.onBoardingSubTitle3,
+          ImageAssets.onboardingLogo3,
+        ),
+        SliderObject(
+          AppStrings.onBoardingTitle4,
+          AppStrings.onBoardingSubTitle4,
+          ImageAssets.onboardingLogo4,
+        ),
+      ];
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorManager.primary,
-      body:const Center(
-        child:  Text(AppStrings.welcome,
-          // style: getRegularStyle(color: ColorManager.white),
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
         ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        itemBuilder: ((context, index) {
+          // return onboardingpage
+        }),
       ),
     );
   }
+}
+
+class OnboardingPage extends StatelessWidget {
+  SliderObject? _sliderObject;
+  OnboardingPage(this._sliderObject, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: AppSize.s40,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(
+            AppPadding.p8,
+          ),
+          child: Text(
+            _sliderObject!.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(
+            AppPadding.p8,
+          ),
+          child: Text(
+            _sliderObject!.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        const SizedBox(
+          height: AppSize.s60,
+        ),
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+  SliderObject(this.title, this.subTitle, this.image);
 }
