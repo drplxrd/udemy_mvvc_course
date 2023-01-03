dfc0597db7f283ce8803bc0e6102aaa851c3100e
drplxrd <kamangasteven00@gmail.com>
2023-01-03 10:51:02 +0200

    Creating Stream controller for input and outputs 	modified:   lib/presentation/onbording/onboarding.dart

--- lib/presentation/onbording/onboarding.dart
+++ lib/presentation/onbording/onboarding.dart
@@ -237,10 +237,3 @@ class OnboardingPage extends StatelessWidget {
     );
   }
 }
-
-class SliderObject {
-  String title;
-  String subTitle;
-  String image;
-  SliderObject(this.title, this.subTitle, this.image);
-}
