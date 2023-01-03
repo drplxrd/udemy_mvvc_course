6c95c7fd2ce99c403097c21c49ab9c24ac5610dd
drplxrd <kamangasteven00@gmail.com>
2023-01-03 10:27:21 +0200

    new file:   lib/presentation/onbording/onboarding_viewmodel.dart

---
+++ lib/presentation/onbording/onboarding_viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:udemy_mvvc_course/presentation/base/baseviewmodel.dart';
+
+class OnBoardingViewModel extends BaseViewModel {
+  //inputs
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
+
+//inputs mean that our view model will receive from our view
+abstract class OnBoardingViewModelInputs {}
+
+//Outputs mean data or results from viewmodel to view
+abstract class OnBoardingViewModelOutputs {}
