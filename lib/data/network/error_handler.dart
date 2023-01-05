fc3966d3f8893ec16f938be32c71b44509aabd50
drplxrd <kamangasteven00@gmail.com>
2023-01-05 15:30:38 +0200

    Staus codes

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -15,3 +15,23 @@ enum DataSource {
   CACHE_ERROR,
   NO_INTERNET_CONNECTION,
 }
+
+class ResponseCode {
+  //API STATUS CODES
+  static const int SUCCESS = 200; //success with data
+  static const int NO_CONTENT = 201; //success with no content
+  static const int BAD_REQUEST = 400; //api rejected the request or badrequest
+  static const int FORBIDDEN = 403; //api rejected request
+  static const int UNAUTHORISED = 401; //credentials unmatched
+  static const int NOT_FOUND = 404; //falure to reach the api, or api not found
+  static const int INTERNAL_SERVER_ERROR = 500; //server crash or arror
+
+  //Local status codes
+  static const int UNKNOWN = -1;
+  static const int CONNECT_TIMEOUT = -2;
+  static const int CANCEL = -3;
+  static const int RECEIVE_TIMEOUT = -5;
+  static const int SEND_TIMEOUT = -6;
+  static const int CACHE_ERROR = -7;
+  static const int NO_INTERNET_CONNECTION = -8;
+}
