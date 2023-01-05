063fd16fc2e59737e838728b82267696053ff228
drplxrd <kamangasteven00@gmail.com>
2023-01-05 11:18:25 +0200

    Extensions 2

---
+++ lib/app/extenstions.dart
@@ -0,0 +1,21 @@
+//Extensions on string
+
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return "";
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullInteger on int? {
+  int orZero() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,38 @@
+//convert the response into non nullable objects
+// ignore_for_file: constant_identifier_names
+
+import 'package:udemy_mvvc_course/app/extenstions.dart';
+import 'package:udemy_mvvc_course/data/responses/responses.dart';
+import '../../domain/model.dart';
+
+const EMPTY = "";
+const ZERO = 0;
+
+extension CustomerResponseMapper on CustomerResponse? {
+  Customer toDomain() {
+    return Customer(
+      this?.id?.orEmpty() ?? EMPTY,
+      this?.name?.orEmpty() ?? EMPTY,
+      this?.numOfNotifications?.orZero() ?? ZERO,
+    );
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse? {
+  Contacts toDomain() {
+    return Contacts(
+      this?.email?.orEmpty() ?? EMPTY,
+      this?.link?.orEmpty() ?? EMPTY,
+      this?.phone?.orEmpty() ?? EMPTY,
+    );
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse? {
+  Authentication toDomain() {
+    return Authentication(
+      this?.contacts.toDomain(),
+      this?.customer?.toDomain(),
+    );
+  }
+}
