6a55c1b10cbba76da92ff5403a2e00063788dfa1
drplxrd <kamangasteven00@gmail.com>
2023-01-12 14:44:38 +0200

    made dio dynamic

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: constant_identifier_names, no_leading_underscores_for_local_identifiers
+// ignore_for_file: constant_identifier_names, no_leading_underscores_for_local_identifiers, prefer_final_fields
 
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:udemy_mvvc_course/app/app_prefs.dart';
 import 'package:udemy_mvvc_course/app/constants.dart';
 
 const String APPLICATION_JSON = "application/json";
@@ -12,15 +13,18 @@ const String AUTHORIZATION = "authorization";
 const String DEFAULT_LANGUAGE = "language";
 
 class DioFactory {
+  AppPreferences _appPreferences;
+
+  DioFactory(this._appPreferences);
   Future<Dio> getDio() async {
     Dio dio = Dio();
-
     int _timeOut = 60 * 1000;
+    String language = await _appPreferences.getAppLanguage();
     Map<String, String> headers = {
       CONTENT_TYPE: APPLICATION_JSON,
       ACCEPT: APPLICATION_JSON,
       AUTHORIZATION: Constant.token,
-      DEFAULT_LANGUAGE: "en", // todo: get language from app prefs
+      DEFAULT_LANGUAGE: language,
     };
 
     dio.options = BaseOptions(
